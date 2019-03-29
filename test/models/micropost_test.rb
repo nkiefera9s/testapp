60bce67f3c1c9e7ad79e433fa3f79fd57334759f
Niklas Kiefer <nkiefer@anynines.com>
2019-03-29 15:56:20 +0100

    weiterführende Tests

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    
+  end
+
+  test "user id should be present" do
+    
+  end 
 end
+# describe MicropostTest do 
+
+#   before(:each) do    
+#         @micropost = MicropostTest.new(@user.microposts.build(content: "Lorem ipsum"))  
+#         @user = users(:michael)      
+#   end
+#   it 'should be valid' do 
+#      @micropost = @user.microposts.build(content: "Lorem ipsum")
+#      assert @micropost.valid?
+#   end 
+
+#   it 'the user id should be present' do
+#     @micropost.user_id = nil
+#     assert_not @micropost.valid?
+#   end
+# end
