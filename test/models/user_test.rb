a8deeb85a3b3f1b7640e8e7be66a5ba64694c6d6
Niklas Kiefer <nkiefer@anynines.com>
2019-03-29 14:41:13 +0100

    Added Tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe User do 
+    it 'the list_student_names method should work correctly' do 
+       student1 = double('student') 
+       student2 = double('student')
+       
+       student1.stub(:name).and_return('John Smith')
+       student2.stub(:name).and_return('Jill Smith') 
+       
+       cr = ClassRoom.new [student1,student2] 
+       expect(cr.list_student_names).to eq('John Smith,Jill Smith') 
+    end 
+ end
 end
